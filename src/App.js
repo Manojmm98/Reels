c69932829f3cd68d7b390f7c9e4388025ed11dce
Manojmm98 <manojmishra98feb@gmail.com>
2021-07-19 16:55:30 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Signup from './Components/Signup';
 import AuthProvider from './Context/AuthProvider';
+import Main from './MaterialUi/Main'
+import Navbar from './MaterialUi/Navbar'
 function App() {
   return (
-      <AuthProvider>
-      <Signup/>
-      </AuthProvider>
+      // <AuthProvider>
+      // <Signup/>
+      // </AuthProvider>
+      //<Main/>
+      <Navbar/>
   );
 }
 
